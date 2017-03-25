d252dc7563713dffd741c2e7ffe9f92db32f5850
Peter Chen <peterchenadded@gmail.com>
2017-03-25 22:32:05 +1100

    got stop working as per tutorial

--- docker/server.js
+++ docker/server.js
@@ -30,4 +30,18 @@ io.on('connection', socket => {
 	socket.on('containers.list', () => {
 		refreshContainers()
 	})
+	socket.on('container.start', args => {
+		const container = docker.getContainer(args.id)
+		if(container) {
+			container.start((err, data) => refreshContainers())
+		}
+	})
+	socket.on('container.stop', args => {
+		const container = docker.getContainer(args.id)
+		if(container) {
+			container.stop((err, data) => refreshContainers())
+		}
+	})
 })
+
+setInterval(refreshContainers, 5000)
